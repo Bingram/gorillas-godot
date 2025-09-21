9aa768933fe4bf3df98abd6e6381bbc7185ec7f3
Brian Ingram <ingram.brian@gmail.com>
2025-09-20 23:37:01 -0700

    playing around with getting player position in order to better understand how to setup tiles relative to player

--- scripts/tile_map_layer.gd
+++ scripts/tile_map_layer.gd
@@ -1,1 +1,11 @@
-extends TileMapLayer
+extends Node2D
+
+@onready var city_tiles: TileMapLayer = $CityTiles
+@onready var player_one: RigidBody2D = $"../Player Left"
+@onready var player_two: RigidBody2D = $"../Player Right"
+
+func _init():
+	pass
+	
+func _ready():
+	pass
